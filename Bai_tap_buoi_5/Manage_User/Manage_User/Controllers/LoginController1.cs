93f73904b88507a50fead70511d8aad36abd2db5
Heulween19 <anayang1907@gmail.com>
2022-11-28 12:50:03 +0700

    add login view

---
+++ Bai_tap_buoi_5/Manage_User/Manage_User/Controllers/LoginController1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Manage_User.Models;
+using IdentityModel.OidcClient;
+
+namespace Manage_User.Controllers
+{
+    public class LoginController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Login(LoginRequest request)
+        {
+            return View();
+        }
+        
+    }
+}
